a081f9e31d526883ce8ac669d3755c887a2cd056
Manoj M J <mmjthefighter@gmail.com>
2026-06-01 07:48:24 +0530

    Add algorithm switchability

--- Nim/AppleNim.cpp
+++ Nim/AppleNim.cpp
@@ -9,97 +9,121 @@
 //
 
 #include <iostream>
-using namespace std;
 #define APPLE_NIM_DEBUG
 #define APPLE_NIM_INFINITY 32000
 #define BESTSCORE 1000
 enum { NONE = -1, COMPUTER = 0, HUMAN = 1 };
+enum { MODULO, NAIVE_MINIMAX, ALPHA_BETA_MINIMAX };
 
 class AppleNim
 {
 private:
 	int m_numTotalApples;
 	int m_turn;
-	int m_useMiniMaxAI;
+	int m_algorithmIndex;
 #ifdef APPLE_NIM_DEBUG
 	int m_maxDepth;
 	int m_nodesSearched;
 #endif
 public:
 	AppleNim(void);
-	void Init(int numApples, int turn, bool useMiniMaxAI = false);
+	void Init(int numApples, int turn, int algorithm);
 	void GameLoop();
 private:
 	int FindBestMoveWithModuloOperator(int numTotalApples);//best method, use this function in production code
-	int FindBestMoveWithSearchTree(int numTotalApples);//just for minimax algorithm demonstration, don't use this function in production code
-	int MiniMax(int numTotalApples, bool maximizingPlayer, int depth, int alpha, int beta);
+	int FindBestMoveWithMiniMaxAlphaBeta(int numTotalApples);//just for minimax algorithm demonstration, don't use this function in production code
+	int FindBestMoveWithMiniMax(int numTotalApples);//just for minimax algorithm demonstration, don't use this function in production code
+	int MiniMaxAlphaBeta(int numTotalApples, bool maximizingPlayer, int depth, int alpha, int beta);
+	int MiniMax(int numTotalApples, bool maximizingPlayer, int depth);
 };
 
-AppleNim::AppleNim(void)
+AppleNim::AppleNim()
 {
 	m_numTotalApples = 21;
 	m_turn = HUMAN;
-	m_useMiniMaxAI = false;
+	m_algorithmIndex = MODULO;
 }
 
-void AppleNim::Init(int numApples, int turn, bool useMiniMaxAI)
+void AppleNim::Init(int numApples, int turn, int algorithm)
 {
 	m_numTotalApples = numApples;
 	m_turn = turn;
-	m_useMiniMaxAI = useMiniMaxAI;
+	m_algorithmIndex = algorithm;
+	switch (algorithm) {
+	case MODULO:
+		std::cout << "Modulo selected\n";
+		break;
+	case NAIVE_MINIMAX:
+		std::cout << "MiniMax selected\n";
+		break;
+	case ALPHA_BETA_MINIMAX:
+		std::cout << "MiniMax+alpha beta pruning selected\n";
+		break;
+	}
 }
 
 void AppleNim::GameLoop()
 {
-	int pick;
+	int pick=1;
 	bool invalidInput = false;
-	cout << "***ABOUT APPLE NIM***" << endl;
-	cout << "A small number of apples are placed on a basket." << endl;
-	cout << "Players can remove 1, 2, 3 or 4 apples in turns." << endl;
-	cout << "Whoever picks the last apple loses the game." << endl;
-	cout << "***START***" << endl;
+	std::cout << "***ABOUT APPLE NIM***\n";
+	std::cout << "A small number of apples are placed on a basket.\n";
+	std::cout << "Players can remove 1, 2, 3 or 4 apples in turns.\n";
+	std::cout << "Whoever picks the last apple loses the game.\n";
+	std::cout << "***START***\n";
 	while (m_numTotalApples > 0)
 	{
 		if (m_turn == HUMAN) {
-			cout << "Number of apples left: [[ " << m_numTotalApples << " ]]" << endl;
-			cout << "***YOUR TURN***" << endl;
+			std::cout << "Number of apples left: [[ " << m_numTotalApples << " ]]\n";
+			std::cout << "***YOUR TURN***\n";
 			do {
-				cout << "Enter your pick: ";
-				cin >> pick;
+				std::cout << "Enter your pick: ";
+				std::cin >> pick;
 				if (invalidInput = (pick < 0 || pick > 4 || (m_numTotalApples - pick) < 0)){
-					cout << "Invalid input" << endl;
+					std::cout << "Invalid input\n";
 				}
 			} while (invalidInput);
 			m_numTotalApples -= pick;
 			m_turn = (m_turn + 1) % 2;
 		}
 		else if (m_turn == COMPUTER) {
-			cout << "Number of apples left: [ " << m_numTotalApples << " ]" << endl;
-			cout << "***COMPUTER'S TURN***" << endl;
-			if (!m_useMiniMaxAI)
+			std::cout << "Number of apples left: [ " << m_numTotalApples << " ]\n";
+			std::cout << "***COMPUTER'S TURN***\n";
+			switch (m_algorithmIndex) {
+			case MODULO:
 				pick = FindBestMoveWithModuloOperator(m_numTotalApples);
-			else
-				pick = FindBestMoveWithSearchTree(m_numTotalApples);
+				break;
+			case NAIVE_MINIMAX:
+				pick = FindBestMoveWithMiniMax(m_numTotalApples);
+				break;
+			case ALPHA_BETA_MINIMAX:
+				pick = FindBestMoveWithMiniMaxAlphaBeta(m_numTotalApples);
+				break;
+			default:
+				std::cout << "Invalid Algorithm\n";
+				return;
+			}
 			m_numTotalApples -= pick;
 			m_turn = (m_turn + 1) % 2;
-			cout << "Computer picks ( ";
+			std::cout << "Computer picks ( ";
 			if (pick != 1)
-				cout << pick << " ) apples." << endl;
+				std::cout << pick << " ) apples.\n";
 			else
-				cout << pick << " ) apple." << endl;
+				std::cout << pick << " ) apple.\n";
 		}
 		if (m_numTotalApples == 0) {
-			cout << "Empty basket! " << endl;
+			std::cout << "Empty basket! \n";
 			if (m_turn == COMPUTER)
-				cout << "You lose." << endl;
+				std::cout << "You lose.\n";
 			else
-				cout << "You win." << endl;
+				std::cout << "You win.\n";
 		}
 	}
 
-	cout << "***Game Over***" << endl;
+	std::cout << "***Game Over***\n";
 }
 
+//-------------------- modulo ------------------------------
 int AppleNim::FindBestMoveWithModuloOperator(int numTotalApples)
 {
 	for (int pick = 4; pick > 0; --pick)
@@ -108,8 +132,8 @@ int AppleNim::FindBestMoveWithModuloOperator(int numTotalApples)
 	return 1;
 }
 
-
-int AppleNim::FindBestMoveWithSearchTree(int numTotalApples)
+//-------------------- mini max with alpha beta pruning ------------------------------
+int AppleNim::FindBestMoveWithMiniMaxAlphaBeta(int numTotalApples)
 {
 #ifdef APPLE_NIM_DEBUG
 	m_maxDepth = 0;
@@ -123,7 +147,7 @@ int AppleNim::FindBestMoveWithSearchTree(int numTotalApples)
 #ifdef APPLE_NIM_DEBUG
 			++m_nodesSearched;
 #endif	
-			int value = MiniMax(numTotalApples, false, 1, -APPLE_NIM_INFINITY, APPLE_NIM_INFINITY);
+			int value = MiniMaxAlphaBeta(numTotalApples, false, 1, -APPLE_NIM_INFINITY, APPLE_NIM_INFINITY);
 			if (value > best) {
 				best = value;
 				bestPick = pick;
@@ -132,12 +156,12 @@ int AppleNim::FindBestMoveWithSearchTree(int numTotalApples)
 		numTotalApples = numTotalApples + pick;
 	}
 #ifdef APPLE_NIM_DEBUG
-	cout << endl << "Searched " << m_nodesSearched << " nodes at a maximum depth of " << m_maxDepth << endl;
+	std::cout << "\nSearched " << m_nodesSearched << " nodes at a maximum depth of " << m_maxDepth << '\n';
 #endif	
 	return bestPick;
 }
 
-int AppleNim::MiniMax(int totalApples, bool maximizingPlayer, int depth, int alpha, int beta)
+int AppleNim::MiniMaxAlphaBeta(int totalApples, bool maximizingPlayer, int depth, int alpha, int beta)
 {
 #ifdef APPLE_NIM_DEBUG
 	if (depth > m_maxDepth)
@@ -163,7 +187,7 @@ int AppleNim::MiniMax(int totalApples, bool maximizingPlayer, int depth, int alp
 #ifdef APPLE_NIM_DEBUG
 				++m_nodesSearched;
 #endif	
-				int value = MiniMax(totalApples, false, depth + 1, alpha, beta);
+				int value = MiniMaxAlphaBeta(totalApples, false, depth + 1, alpha, beta);
 				if (value > best) {
 					best = value;
 				}
@@ -186,7 +210,7 @@ int AppleNim::MiniMax(int totalApples, bool maximizingPlayer, int depth, int alp
 #ifdef APPLE_NIM_DEBUG
 				++m_nodesSearched;
 #endif	
-				int value = MiniMax(totalApples, true, depth + 1, alpha, beta);
+				int value = MiniMaxAlphaBeta(totalApples, true, depth + 1, alpha, beta);
 				if (value < best) {
 					best = value;
 				}
@@ -203,11 +227,97 @@ int AppleNim::MiniMax(int totalApples, bool maximizingPlayer, int depth, int alp
 	}
 }
 
+//-------------------- naive mini max ------------------------------
+int AppleNim::FindBestMoveWithMiniMax(int numTotalApples)
+{
+#ifdef APPLE_NIM_DEBUG
+	m_maxDepth = 0;
+	m_nodesSearched = 0;
+#endif
+	int best = -APPLE_NIM_INFINITY;
+	int bestPick = 0;
+	for (int pick = 4; pick > 0; --pick) {
+		numTotalApples = numTotalApples - pick;
+		if (numTotalApples >= 0) {
+#ifdef APPLE_NIM_DEBUG
+			++m_nodesSearched;
+#endif	
+			int value = MiniMax(numTotalApples, false, 1);
+			if (value > best) {
+				best = value;
+				bestPick = pick;
+			}
+		}
+		numTotalApples = numTotalApples + pick;
+	}
+#ifdef APPLE_NIM_DEBUG
+	std::cout << "\nSearched " << m_nodesSearched << " nodes at a maximum depth of " << m_maxDepth << '\n';
+#endif	
+	return bestPick;
+}
+
+int AppleNim::MiniMax(int totalApples, bool maximizingPlayer, int depth)
+{
+#ifdef APPLE_NIM_DEBUG
+	if (depth > m_maxDepth)
+		m_maxDepth = depth;
+#endif
+	if (totalApples == 0) {
+		if (maximizingPlayer)
+			return (BESTSCORE - depth); // computer is presented with empty basket
+		else
+			return -(BESTSCORE - depth);//human is presented with empty basket
+	}
+	else if (totalApples == 1) {
+		if (maximizingPlayer)
+			return -(BESTSCORE - depth);//computer is presented with one apple
+		else
+			return BESTSCORE - depth;//human is presented with one apple
+	}
+	else if (maximizingPlayer) {
+		int best = -APPLE_NIM_INFINITY;
+		for (int pick = 4; pick > 0; --pick) {
+			totalApples = totalApples - pick;
+			if (totalApples >= 0) {
+#ifdef APPLE_NIM_DEBUG
+				++m_nodesSearched;
+#endif	
+				int value = MiniMax(totalApples, false, depth + 1);
+				if (value > best) {
+					best = value;
+				}
+			}
+			totalApples = totalApples + pick;
+		}
+		return best;
+	}
+	else
+	{
+		int best = APPLE_NIM_INFINITY;
+		for (int pick = 4; pick > 0; --pick) {
+			totalApples = totalApples - pick;
+			if (totalApples >= 0) {
+#ifdef APPLE_NIM_DEBUG
+				++m_nodesSearched;
+#endif	
+				int value = MiniMax(totalApples, true, depth + 1);
+				if (value < best) {
+					best = value;
+				}
+			}
+			totalApples = totalApples + pick;
+		}
+		return best;
+	}
+}
+
 int main()
 {
 	AppleNim n;
-	n.Init(26, HUMAN, true);
-	//n.Init(26, HUMAN, false);
+	//n.Init(26, HUMAN, MODULO);
+	n.Init(26, HUMAN, ALPHA_BETA_MINIMAX);
+	//n.Init(26, HUMAN, NAIVE_MINIMAX);
+	
 	n.GameLoop();
 	return 0;
 }
